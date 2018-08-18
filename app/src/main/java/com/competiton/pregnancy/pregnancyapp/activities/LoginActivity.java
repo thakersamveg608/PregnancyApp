5c6ef5a250cdae3aa879874a7d6e70497642ddbb
Vrihas123 <vrihas007@gmail.com>
2018-08-18 10:45:12 +0530

    colors added

---
+++ app/src/main/java/com/competiton/pregnancy/pregnancyapp/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.competiton.pregnancy.pregnancyapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.competiton.pregnancy.pregnancyapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
